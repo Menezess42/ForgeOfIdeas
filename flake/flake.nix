b3059298e6bff70a2cf2fd437bedc3e4a6866311
Menezess42 <arielmenezess@hotmail.com>
2025-09-15 14:30:48 -0300

    Change flake

--- flake/flake.nix
+++ flake/flake.nix
@@ -4,14 +4,14 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    essentials.url = "path:/mnt/hdmenezess42/GitProjects/flakeEssentials";
+    # essentials.url = "path:/mnt/hdmenezess42/GitProjects/flakeEssentials";
   };
 
-  outputs = { self, nixpkgs, flake-utils, essentials }:
+  outputs = { self, nixpkgs, flake-utils, }:#essentials }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
-        baseShell = essentials.devShells.${system}.default;
+        # baseShell = essentials.devShells.${system}.python;
       in {
         devShell = pkgs.mkShell {
           name = "ForgeOfIdeas-shell";
@@ -20,7 +20,7 @@
             pkgs.nodejs_22   # Node.js moderno, útil para servir e ferramentas JS
             pkgs.yarn        # ou pnpm/npm, se preferir
             pkgs.git
-          ] ++ baseShell.buildInputs;
+          ]; # ++ baseShell.buildInputs;
 
           shellHook = ''
             echo "🔨 Welcome to The ForgeOfIdeas!"
