ae861eee67cc04dc34f54de20834717c31a28e8c
Menezess42 <arielmenezess@hotmail.com>
2025-09-23 16:23:59 -0300

    Change flake to include react and tailwind

--- flake/flake.nix
+++ flake/flake.nix
@@ -1,32 +1,67 @@
 {
-  description = "ForgeOfIdeas - Cozy Pixel Art Project Tracker";
+  description = "ForgeOfIdeas - Cozy Pixel Art Project Tracker (React + Tailwind DevShell)";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    # essentials.url = "path:/mnt/hdmenezess42/GitProjects/flakeEssentials";
   };
 
-  outputs = { self, nixpkgs, flake-utils, }:#essentials }:
+  outputs = { self, nixpkgs, flake-utils, }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
-        # baseShell = essentials.devShells.${system}.python;
       in {
         devShell = pkgs.mkShell {
           name = "ForgeOfIdeas-shell";
 
           buildInputs = [
-            pkgs.nodejs_22   # Node.js moderno, útil para servir e ferramentas JS
-            pkgs.yarn        # ou pnpm/npm, se preferir
+            pkgs.nodejs_22
+            pkgs.yarn
             pkgs.git
-          ]; # ++ baseShell.buildInputs;
+            pkgs.tailwindcss
+            pkgs.postcss
+            pkgs.autoprefixer
+            pkgs.vite
+          ];
 
           shellHook = ''
             echo "🔨 Welcome to The ForgeOfIdeas!"
-            echo "Use 'npx serve .' to run the site local"
+            echo "Run 'yarn create vite my-app --template react' to bootstrap a new project"
+            echo "Then: cd my-app && echo 'Ready to hack with Tailwind + Vite 🎨'"
           '';
         };
       }
     );
 }
+# {
+#   description = "ForgeOfIdeas - Cozy Pixel Art Project Tracker";
+#
+#   inputs = {
+#     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+#     flake-utils.url = "github:numtide/flake-utils";
+#     # essentials.url = "path:/mnt/hdmenezess42/GitProjects/flakeEssentials";
+#   };
+#
+#   outputs = { self, nixpkgs, flake-utils, }:#essentials }:
+#     flake-utils.lib.eachDefaultSystem (system:
+#       let
+#         pkgs = import nixpkgs { inherit system; };
+#         # baseShell = essentials.devShells.${system}.python;
+#       in {
+#         devShell = pkgs.mkShell {
+#           name = "ForgeOfIdeas-shell";
+#
+#           buildInputs = [
+#             pkgs.nodejs_22   # Node.js moderno, útil para servir e ferramentas JS
+#             pkgs.yarn        # ou pnpm/npm, se preferir
+#             pkgs.git
+#           ]; # ++ baseShell.buildInputs;
+#
+#           shellHook = ''
+#             echo "🔨 Welcome to The ForgeOfIdeas!"
+#             echo "Use 'npx serve .' to run the site local"
+#           '';
+#         };
+#       }
+#     );
+# }
