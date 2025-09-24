83c2c52339c1e6a9f200feb54ff3d40cd4e70498
Menezess42 <arielmenezess@hotmail.com>
2025-09-24 11:31:24 -0300

    Mergeing the flake with the flakeEssentials for js

--- ForgeOfIdeas/src/App.jsx
+++ ForgeOfIdeas/src/App.jsx
@@ -1,16 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Shelf from './components/Shelf.jsx'
+import './Styles/App.css'
+import Shelf from './Components/Shelf.jsx'
 
 function App() {
 
   return (
       <>
-      <Header />
-      < Content />
-      <Footer />
+      {/* <Header /> */}
+      {/* < Content /> */}
+      {/* <Footer /> */}
       </>
   )
 }
--- flake/flake.nix
+++ flake/flake.nix
@@ -4,12 +4,14 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    essentials.url = "path:/mnt/hdmenezess42/GitProjects/flakeEssentials";
   };
 
-  outputs = { self, nixpkgs, flake-utils, }:
+  outputs = { self, nixpkgs, flake-utils, essentials}:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
+        baseShell = essentials.devShell.${system}.js;
       in {
         devShell = pkgs.mkShell {
           name = "ForgeOfIdeas-shell";
@@ -22,7 +24,7 @@
             pkgs.nodePackages.postcss
             pkgs.autoprefixer
             pkgs.vite
-          ];
+          ] ++ baseShell.buildInputs;
 
           shellHook = ''
             echo "🔨 Welcome to The ForgeOfIdeas!"
