19d94955e35ff1d7236dd679cb37b04d97f5726a
Menezess42 <arielmenezess@hotmail.com>
2025-09-23 21:04:58 -0300

    Change components to Components

--- ForgeOfIdeas/src/App.jsx
+++ ForgeOfIdeas/src/App.jsx
@@ -8,29 +8,9 @@ function App() {
 
   return (
       <>
-      <div className="flex flex-col min-h-screen">
-      {/* Topo */}
-      <header className="bg-gray-800 text-white p-4 text-center text-xl font-bold">
-      Forge Of Ideas
-      </header>
-
-      {/* Meio */}
-      <main className="flex flex-1">
-      {/* Estante */}
-      <Shelf />
-
-      {/* Bigorna */}
-      {/* <Anvil /> */}
-
-      {/* Forja */}
-      {/* <Forge /> */}
-      </main>
-
-      {/* Rodapé */}
-      <footer className="bg-gray-900 text-white p-4 text-center">
-      Contact me | About | etc
-      </footer>
-      </div>
+      <Header />
+      < Content />
+      <Footer />
       </>
   )
 }
