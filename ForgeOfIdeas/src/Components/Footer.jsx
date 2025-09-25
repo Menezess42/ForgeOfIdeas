7ee1c79d139c5077b7b8eef54c926184ba0d80b8
Menezess42 <arielmenezess@hotmail.com>
2025-09-24 21:57:39 -0300

    God bless this project

--- ForgeOfIdeas/src/Components/Footer.jsx
+++ ForgeOfIdeas/src/Components/Footer.jsx
@@ -1,8 +1,51 @@
 import '../Styles/Components.css'
+import twitch from '/twitch.png'
+import github from '/github.png'
+import ds from '../Assets/Forgrin.png'
+import yt from '/yt.png'
+import insta from '/instagram.png'
 export default function Content(){
 return (
-    <footer>
-    <h3>Contact Us</h3>
+    <footer className="Footer">
+      <div
+        className="Brand"
+        onClick={() => window.open("https://github.com/Menezess42", "_blank")}
+      >
+        <img className="Social" src={github} alt="logo" />
+        <h3>GitHub</h3>
+      </div>
+
+      <div
+        className="Brand"
+        onClick={() => window.open("https://www.instagram.com/dwarf_software", "_blank")}
+      >
+        <img className="Social" src={insta} alt="logo" />
+        <h3>Insta</h3>
+      </div>
+
+      <div
+        className="Brand"
+        onClick={() => window.open("https://github.com/Menezess42", "_blank")}
+      >
+        <img className="Social" src={ds} alt="logo" />
+        <h3>DwarfSoftware</h3>
+      </div>
+
+      <div
+        className="Brand"
+        onClick={() => window.open("https://youtube.com/@Dwarf_Software", "_blank")}
+      >
+        <img className="Social" src={yt} alt="logo" />
+        <h3>YouTube</h3>
+      </div>
+
+      <div
+        className="Brand"
+        onClick={() => window.open("https://twitch.tv/dwarf_software", "_blank")}
+      >
+        <img className="Social" src={twitch} alt="logo" />
+        <h3>Twitch</h3>
+      </div>
     </footer>
 );
 }
--- ForgeOfIdeas/src/Components/Header.jsx
+++ ForgeOfIdeas/src/Components/Header.jsx
@@ -1,9 +1,15 @@
 import '../Styles/Components.css'
 import Shelf from './Shelf.jsx'
+import anvil from '../Assets/anvil.png'
+import shelf from '../Assets/shellf.png'
 export default function Content(){
 return (
-    <header>
+    <header className="Header">
+    <div className="Brand">
+    <img className="Logo-1" src={anvil} alt="logo" />
     <h1>Forge Of Ideas</h1>
+    <img className="Logo-2" src={shelf} alt="logo" />
+    </div>
     </header>
 );
 }
