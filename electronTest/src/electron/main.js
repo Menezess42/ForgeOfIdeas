c9a3fb6ab79ac8baa1ee0ebb6d9ff55c8f570f53
Menezess42 <arielmenezess@hotmail.com>
2025-09-29 16:25:30 -0300

    The react sunshine shows its face

--- electronTest/src/electron/main.js
+++ electronTest/src/electron/main.js
@@ -1,10 +1,8 @@
 import {app, BrowserView, BrowserWindow} from 'electron'
 import path from 'path';
 
-app.on("read", ()=>{
-    const mainWindow = new BrowserWindow(
-        width: 800,
-        height: 600
-    );
+app.on("ready", ()=>{
+    const mainWindow = new BrowserWindow({
+    });
     mainWindow.loadFile(path.join(app.getAppPath(),'/dist-react/index.html'));
 });
