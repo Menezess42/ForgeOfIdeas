8ee7eeacf022e5922cf08c8e2479e1c25e41ba2c
Menezess42 <arielmenezess@hotmail.com>
2025-09-28 23:03:22 -0300

    Small victory, a different bug report

--- electronTest/src/electron/main.js
+++ electronTest/src/electron/main.js
@@ -2,6 +2,9 @@ import {app, BrowserView, BrowserWindow} from 'electron'
 import path from 'path';
 
 app.on("read", ()=>{
-    const mainWindow = new BrowserWindow();
+    const mainWindow = new BrowserWindow(
+        width: 800,
+        height: 600
+    );
     mainWindow.loadFile(path.join(app.getAppPath(),'/dist-react/index.html'));
 });
