91c3ca8ce81094925674f854f58b8d2603df4778
Menezess42 <arielmenezess@hotmail.com>
2025-09-30 12:09:21 -0300

    Electron development is more fun in nixOS because is more challenge

--- electronApp/src/ui/App.tsx
+++ electronApp/src/ui/App.tsx
@@ -12,7 +12,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>AAAAAAAA</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -22,7 +22,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      God Is good All the time
       </p>
     </>
   )
