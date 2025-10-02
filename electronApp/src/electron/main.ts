bb2da343e17adb846fcff0abe59a3e1dfb8b9038
Menezess42 <arielmenezess@hotmail.com>
2025-10-02 16:34:22 -0300

    so far so good tks to God

--- electronApp/src/electron/main.ts
+++ electronApp/src/electron/main.ts
@@ -1,8 +1,8 @@
 // /src/electron/main.ts
-import {app, BrowserWindow} from 'electron';
+import {app, BrowserWindow, ipcMain} from 'electron';
 import path from 'path';
 import {isDev} from  './util.js';
-import { pollResources } from './resourceManager.js';
+import { getStaticData, pollResources } from './resourceManager.js';
 import { getPreloadPath } from './pathResolver.js';
 // type test = string;
 
@@ -20,4 +20,8 @@ app.on("ready", () => {
     }
 
     pollResources(mainWindow);
+
+    ipcMain.handle("getStaticData", ()=>{
+        return getStaticData();
+    })
 })
