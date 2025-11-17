a1c0cef4290f05b3fc6a7c5224ce6761e8857246
Menezess42 <arielmenezess@hotmail.com>
2025-11-17 14:12:04 -0300

    :)

--- ForgeOfIdeas_safeInSouth/dist-electron/preload.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// ForgeOfIdeas/src/electron/preload.ts
-const electron_1 = require("electron");
-console.log("⚙️ Preload carregado com sucesso");
-electron_1.contextBridge.exposeInMainWorld("electronAPI", {
-    saveIdea: (data) => electron_1.ipcRenderer.invoke("save-idea", data)
-});
--- ForgeOfIdeas_safeInSouth/src/electron/preload.ts
+++
@@ -1,7 +0,0 @@
-// ForgeOfIdeas/src/electron/preload.ts
-import { contextBridge, ipcRenderer } from "electron";
-
-console.log("⚙️ Preload carregado com sucesso");
-contextBridge.exposeInMainWorld("electronAPI", {
-  saveIdea: (data: any) => ipcRenderer.invoke("save-idea", data)
-});
