24d9d5319187a3269346f7c026adc7579fc0903d
Menezess42 <arielmenezess@hotmail.com>
2025-12-04 18:37:30 -0300

    flake restructure

--- nix/devShell.nix
+++ nix/devShell.nix
@@ -1,34 +1,22 @@
-{
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-    essentials.url = "path:/mnt/hdmenezess42/GitProjects/flakeEssentials";
-  };
+{ pkgs, system, essentials }:
 
-  outputs = { self, nixpkgs, flake-utils, essentials}:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        baseShell = essentials.devShells.${system}.js;
-        programs.nix-ld.enable = true;
-      in {
-        devShell = pkgs.mkShell rec{
-          name = "ForgeOfIdeas-shell";
+let
+  baseShell = essentials.devShells.${system}.js;
+in
+pkgs.mkShell rec {
+  name = "ForgeOfIdeas-shell";
 
-          buildInputs = [
-            pkgs.nodejs_22
-            pkgs.yarn
-            pkgs.git
-            pkgs.tailwindcss
-            pkgs.nodePackages.postcss
-            pkgs.autoprefixer
-            pkgs.electron
-          ] ++ baseShell.buildInputs;
+  buildInputs = [
+    pkgs.nodejs_22
+    pkgs.yarn
+    pkgs.git
+    pkgs.tailwindcss
+    pkgs.nodePackages.postcss
+    pkgs.autoprefixer
+    pkgs.electron
+  ] ++ baseShell.buildInputs;
 
-          shellHook = ''
-          echo "🔨 Welcome to The ForgeOfIdeas!"
-                  '';
-        };
-      }
-    );
+  shellHook = ''
+    echo "🔨 Welcome to The ForgeOfIdeas!"
+  '';
 }
