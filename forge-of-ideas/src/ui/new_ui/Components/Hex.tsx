72d3a7e5a50113e1ef42793c9b8c3f07082abac3
Menezess42 <arielmenezess@hotmail.com>
2026-03-30 14:24:44 -0300

    style: Start config the shelf layout

---
+++ forge-of-ideas/src/ui/new_ui/Components/Hex.tsx
@@ -0,0 +1,58 @@
+import '../styles/hex.css';
+
+type HexProps = {
+    size?: number;
+    color?: string;
+    label?: string;
+    stroke?: string;
+    strokeWidth?: number;
+};
+
+export default function Hex({
+    size = 60,
+    color = "#ff6600",
+    label,
+    stroke,
+   strokeWidth,
+}: HexProps) {
+    const height = size;
+    const width = size;
+
+    return (
+        <div style={{ width, height, position: "relative" }}>
+            <svg
+                viewBox="0 0 100 100"
+                width="100%"
+                height="100%"
+                preserveAspectRatio="xMidYMid meet"
+            >
+                <polygon
+                    points="
+                    50,0
+                    93.3,25
+                    93.3,75
+                    50,100
+                    6.7,75
+                    6.7,25
+                    "
+                    fill={color}
+                    stroke={stroke}
+                    strokeWidth={strokeWidth}
+                />
+            </svg>
+
+            {label && (
+                <div
+                    style={{
+                        position: "absolute",
+                        top: "50%",
+                        left: "50%",
+                        transform: "translate(-50%, -50%)",
+                    }}
+                >
+                    {label}
+                </div>
+            )}
+        </div>
+    );
+}
--- forge-of-ideas/src/ui/new_ui/Components/Shelf.tsx
+++ forge-of-ideas/src/ui/new_ui/Components/Shelf.tsx
@@ -1,7 +1,29 @@
 import '../styles/tokens.css';
+import '../styles/shelf.css';
+import Hex from './Hex.tsx';
 
 export default function Shelf(){
 
     return (
+        <main className="shelf-grid-container">
+            <div className="row v-indicators">
+                <div className="hex-grid">
+                    <Hex size={80} color="#ff5a1f" label="10" className="hex-column"/>
+                    <Hex size={80} color="#00c2b2" label="05" className="hex-column"/>
+                    <Hex size={80} color="#3a4f66" label="05" className="hex-column"/>
+                </div>
+            </div>
+            <div className="row cards"></div>
+            <div className="row buttons">
+                <div className="hex-grid hex-buttons">
+                    <Hex size={70} color="#22303c" className="hex-column" 
+                    stroke="#8A9BB0"/>
+                    <Hex size={70} color="#22303c" className="hex-column"
+                    stroke="#8A9BB0"/>
+                    <Hex size={70} color="#22303c" className="hex-column"
+                    stroke="#8A9BB0"/>
+                </div>
+            </div>
+        </main>
     );
 }
